e50a4ada36d4fb7402679a4c1d2c31de0979df0b
Samuel <s-bridge@live.co.uk>
2015-07-16 23:37:02 +0100

    [Insta] - Reworked to use https

--- app/controller/states/instagram.js
+++ app/controller/states/instagram.js
@@ -1,41 +1,68 @@
-var request = require('koa-request'),
-	Q = require('q'),
+var Q = require('q'),
+	https = require('https'),
 	self = this;
 
-var deferred = Q.defer();
-
 exports.init = function(app) {
-
+	
 	app.use(instagram);
 
 }
 
 var instagram = function *(next) {
-
+	
 	yield getInstagram(this);
 	yield next;
 
 }
 
-var fetchInsta = function(data, process) {
+var complete = function(data, process) {
 
 	process.state.instagram = data;
 
 }
 
-var getInstagram = function *(process) {
+var getInstagram = function(process) {
+	
+	var deferred = Q.defer();
 
 	var accessToken = '414143281.467ede5.b2f838f87a0b418e9d1b7fa21a6d7135',
-		response = yield request({
+		options = {
+		
+			hostname: 'api.instagram.com',
+			path: '/v1/users/self/media/recent?access_token='+accessToken,
+			method: 'GET'
 
-		url : 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + accessToken
+		};
+
+	var request = https.request(options, function(response) {
+		
+		var body = '';
+
+		console.log(response.statusCode)
+
+		response.on('data', function(data) {
+
+			body += data;
+
+		});
+
+		response.on('end', function() {
+
+			var info = JSON.parse(body).data;
+			deferred.resolve(complete(info, process));
+
+		});
+
+		response.on('error', function(err) {
+
+			console.log(err);
+
+		});
 
 	});
 
-	console.log('fetch');
-	
-	var info = JSON.parse(response.body);
+	request.end();
 
-	deferred.resolve(fetchInsta(info.data, process));
+	return deferred.promise;
 
 }
--- server/bin/www.js
+++ server/bin/www.js
@@ -1,5 +1,8 @@
 var koa = require('koa'),
 	static = require('koa-static'),
+	conditional = require('koa-conditional-get'),
+	staticCache = require('koa-static-cache'),
+	etag = require('koa-etag'),
 	controller = require('../controller'),
 	routing = require('../routing'),
 	states = require(__dirname+'/../../app/controller/states/states.js')
@@ -18,7 +21,7 @@ app.use(function *(next) {
 	catch(err) {
 
 		this.status = err.status || 500;
-		this.body = this.status + " - Internal Server Error, ooops :(";
+		this.body = this.status + " - Internal Server Error, ooops :(\n\n" + err;
 
 		console.log(err);
 
@@ -26,11 +29,20 @@ app.use(function *(next) {
 
 });
 
-app.use(logger());
-app.use(static(__dirname + '/../../app'));
+var files = {};
 
-states.init(app);
+app.use(logger());
+app.use(staticCache(__dirname + '/../../app'), {
+
+	maxAge: 24 * 60 * 60
+
+});
+app.use(static(__dirname + '/../../app'));
+app.use(conditional());
+app.use(etag());
+
 controller.init(app);
+states.init(app, routing);
 routing.init(app);
 
 
