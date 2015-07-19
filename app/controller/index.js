903d17118cc119a8ce6b33ffd4ed6f1f572a4213
Samuel <s-bridge@live.co.uk>
2015-07-19 23:33:55 +0100

    [Controller] - Index, removed old code

--- app/controller/index.js
+++ app/controller/index.js
@@ -8,33 +8,3 @@ exports.params = function(page) {
 	}
 
 }
-
-// exports.post = function *(next) {
-
-// 	try {
-
-// 		yield this.render('index', {
-
-// 	 		view: this.page['index'] ? this.page['index'] : this.page['default'],
-// 	 		page: 'index',
-// 	 		response: this.request.body
-
-// 	 	});
-
-// 	}
-
-// 	catch(err) {
-
-// 		if(err.syscall === 'open') {
-
-// 			this.body = err.message;
-
-// 		} else {
-
-// 			this.status = 404;
-
-// 		}
-
-// 	}
-
-// }
