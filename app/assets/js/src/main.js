1a0189e74e910053ca382c5538d558f1c193f902
Samuel <s-bridge@live.co.uk>
2015-07-20 15:30:27 +0100

    [Performance] - Images added in

--- app/assets/js/src/main.js
+++ app/assets/js/src/main.js
@@ -11,17 +11,62 @@ window.propFuncs = {
 
 	},
 
-	images__ready : function() {
+	isVisible__ready : function() {
 
-		var images = $('img[data-src]');
+		var self = this;
 
-		for(var i = 0; i < images.length; i++) {
+		self.init = function(element) {
 
-			var image = $(images[i]);
+			if (!element || 1 !== element.nodeType) return false;
 
-			image.attr('src', image.data('src'));
+			var html = document.documentElement,
+				r = element.getBoundingClientRect(),
+				h = html.clientHeight,
+				w = html.clientWidth;
+
+			if (!!r && r.bottom >= 0 && r.right >= 0 && r.top <= h && r.left <= w) return true;
+
+		};
+
+		self.init();
+
+	},
+
+	bg__ready__scroll : function() {
+
+		var bgImg = document.querySelectorAll('.js-bg');
+
+		for(var i = 0; i < bgImg.length; i++) {
+
+			var img = $(bgImg[i]);
+
+			if(propCore['isVisible'].init(bgImg[i])) {
+
+				if(!img.data('img')) continue;
+
+				img.css({
+
+					'background-image' : 'url("' + img.data('img') + '")'
+
+				}).attr('data-img', '');
+
+				if(img.attr('data-img') == '') {
+					img.removeAttr('data-img');
+				}
+
+			}
 
 		}
+
+	},
+
+	imager__ready : function() {
+
+		return new Imager('.js-imager', {
+
+			lazyload: true
+
+		});
 
 	}
 
